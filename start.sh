12f5441e960f25aad0582744ebcdceba8ff3ba81
MootComb <149808245+MootComb@users.noreply.github.com>
2025-04-03 21:09:32 +0500

    Update start.sh
    Signed-off-by: MootComb <149808245+MootComb@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ SUDO=$(command -v sudo)
 
 # Функция для проверки и установки пакетов
 install_dependencies() {
-    echo "Необходимые пакеты не найдены. Устанавливаю их..."фф
+    echo "Необходимые пакеты не найдены. Устанавливаю их..."
     $SUDO apt-get update
     $SUDO apt-get install -y dialog git
 }
@@ -28,10 +28,12 @@ CLONE_DIR="/tmp/MootComb"
 
 # Очищаем временную директорию, если она существует
 if [ -d "$CLONE_DIR" ]; then
+    echo "Очищаем временную директорию..."
     rm -rf "$CLONE_DIR"
 fi
 
 # Клонируем репозиторий
+echo "Клонируем репозиторий..."
 git clone "$REPO_URL" "$CLONE_DIR"
 
 # Переходим в директорию с скриптами
@@ -48,10 +50,18 @@ show_menu() {
         CHOICES=()
         current_dir=$(pwd)
 
-        # Добавляем поддиректории
+        # Добавляем поддиректории, если мы не в /tmp/MootComb
         for DIR in "${DIRECTORIES[@]}"; do
             if [ -d "$DIR" ]; then
-                CHOICES+=("$DIR" "$DIR")
+                # Проверяем, не находимся ли мы в /tmp/MootComb
+                if [ "$current_dir" != "$CLONE_DIR" ]; then
+                    CHOICES+=("$DIR" "$DIR")
+                elif [ "$current_dir" == "$CLONE_DIR" ]; then
+                    # Если мы в /tmp/MootComb, добавляем только поддиректории
+                    if [ "$DIR" != "$CLONE_DIR" ]; then
+                        CHOICES+=("$DIR" "$DIR")
+                    fi
+                fi
             fi
         done
 
@@ -74,7 +84,7 @@ show_menu() {
         fi
 
         # Используем dialog для выбора файла или директории
-        SELECTED_ITEM=$(dialog --title "Выберите скрипт или директорию v1.0" --menu "Выберите один из следующих элементов:" 15 50 10 "${CHOICES[@]}" 3>&1 1>&2 2>&3)
+        SELECTED_ITEM=$(dialog --title "Выберите скрипт или директорию v1.0" --menu "Выберите один из следующих элементов:" 15 50 10 "${CHOICES[@]}" --center 3>&1 1>&2 2>&3)
 
         # Проверяем, был ли выбран элемент
         if [ $? -ne 0 ]; then
@@ -88,6 +98,7 @@ show_menu() {
         elif [ -d "$SELECTED_ITEM" ]; then
             # Если выбрана директория, переходим в нее
             cd "$SELECTED_ITEM" || exit
+            echo "Вы находитесь в директории: $SELECTED_ITEM"
         else
             # Проверяем, существует ли выбранный скрипт
             if [ -f "$SELECTED_ITEM" ]; then
