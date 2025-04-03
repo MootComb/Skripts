2d738ce7fe79140280664f1787a4d44e4b829677
MootComb <149808245+MootComb@users.noreply.github.com>
2025-04-04 02:24:43 +0500

    Update start.sh
    Signed-off-by: MootComb <149808245+MootComb@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright (c) 2025 MootComb
+# Author: MootComb
+# License: Apache License 2.0
+# https://github.com/MootComb/Skripts/blob/main/LICENSE
+# Source: https://github.com/MootComb/Skripts
+
 SUDO=$(command -v sudo)
 
 install_dependencies() {
