312c264704fa77f2a022a41707d4286fda1d622c
MootComb <149808245+MootComb@users.noreply.github.com>
2025-04-04 02:25:45 +0500

    Update repoclear.sh
    Signed-off-by: MootComb <149808245+MootComb@users.noreply.github.com>

--- danger/repoclear.sh
+++ danger/repoclear.sh
@@ -1,2 +1,9 @@
 #!bin/sh
+
+# Copyright (c) 2025 MootComb
+# Author: MootComb
+# License: Apache License 2.0
+# https://github.com/MootComb/Skripts/blob/main/LICENSE
+# Source: https://github.com/MootComb/Skripts
+
 echo "очистка"
