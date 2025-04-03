d289d0d34f877d25fe208f9f4242a3b46e1e4d99
MootComb <149808245+MootComb@users.noreply.github.com>
2025-04-04 02:24:58 +0500

    Update configview.sh
    Signed-off-by: MootComb <149808245+MootComb@users.noreply.github.com>

--- proxmox/lxc/configview.sh
+++ proxmox/lxc/configview.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright (c) 2025 MootComb
+# Author: MootComb
+# License: Apache License 2.0
+# https://github.com/MootComb/Skripts/blob/main/LICENSE
+# Source: https://github.com/MootComb/Skripts
+
 # Проверяем, установлены ли необходимые утилиты
 if ! command -v pct &> /dev/null; then
     echo "Утилита pct не найдена! Убедитесь, что Proxmox установлен."
