ab5591b95a286a08626db41aa84d6a99cb706c55
MootComb <149808245+MootComb@users.noreply.github.com>
2025-04-03 20:05:49 +0500

    Update lxcview.sh
    Signed-off-by: MootComb <149808245+MootComb@users.noreply.github.com>

--- proxmox/lxcview.sh
+++ proxmox/lxcview.sh
@@ -25,12 +25,14 @@ IFS=$'\n' read -r -d '' -a container_array <<< "$containers"
 
 # Создаем список для dialog
 options=()
-for container in "${container_array[@]}"; do
-    options+=("$container" "$container")
+for container_id in "${container_array[@]}"; do
+    # Получаем имя контейнера
+    container_name=$(pct config "$container_id" | grep "^hostname:" | awk '{print $2}')
+    options+=("$container_id" "$container_name")
 done
 
 # Выводим список контейнеров с помощью dialog
-selected_container=$(dialog --title "Выберите LXC контейнер" --menu "Выберите контейнер для редактирования:" 15 50 10 "${options[@]}" 3>&1 1>&2 2>&3)
+selected_container_id=$(dialog --title "Выберите LXC контейнер" --menu "Выберите контейнер для редактирования:" 15 50 10 "${options[@]}" 3>&1 1>&2 2>&3)
 
 # Проверка на отмену
 exit_status=$?
@@ -40,7 +42,7 @@ if [ $exit_status != 0 ]; then
 fi
 
 # Открываем конфигурационный файл выбранного контейнера в nano
-config_file="/etc/pve/lxc/$selected_container.conf"
+config_file="/etc/pve/lxc/$selected_container_id.conf"
 if [ -f "$config_file" ]; then
     nano "$config_file"
 else
