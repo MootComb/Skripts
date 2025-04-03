ece3bc602eec367df4dd3673f5ef128f5a7f5126
MootComb <149808245+MootComb@users.noreply.github.com>
2025-04-04 02:26:07 +0500

    Update autostart.sh
    Signed-off-by: MootComb <149808245+MootComb@users.noreply.github.com>

--- debian/autostart.sh
+++ debian/autostart.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright (c) 2025 MootComb
+# Author: MootComb
+# License: Apache License 2.0
+# https://github.com/MootComb/Skripts/blob/main/LICENSE
+# Source: https://github.com/MootComb/Skripts
+
 # Переменная для sudo
 SUDO=$(command -v sudo || echo "")
 
