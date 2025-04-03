d500a661a4ad3f13ac45128c655ecf63cc87dffb
MootComb <149808245+MootComb@users.noreply.github.com>
2025-04-04 02:26:16 +0500

    Update sftp.sh
    Signed-off-by: MootComb <149808245+MootComb@users.noreply.github.com>

--- debian/sftp.sh
+++ debian/sftp.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright (c) 2025 MootComb
+# Author: MootComb
+# License: Apache License 2.0
+# https://github.com/MootComb/Skripts/blob/main/LICENSE
+# Source: https://github.com/MootComb/Skripts
+
 # Функция для отображения сообщений об ошибках
 error() {
     echo "$1" >&2
