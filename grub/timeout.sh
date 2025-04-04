bd803de470e7dc372ea91ca90a245353892c017c
MootComb <149808245+MootComb@users.noreply.github.com>
2025-04-04 11:25:42 +0500

    Create timeout.sh
    Signed-off-by: MootComb <149808245+MootComb@users.noreply.github.com>

---
+++ grub/timeout.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Функция для проверки, является ли введенное значение числом
+is_number() {
+    if [[ $1 =~ ^[0-9]+$ ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Используем dialog для ввода количества секунд
+while true; do
+    # Отображаем диалоговое окно для ввода
+    delay=$(dialog --stdout --inputbox "Введите количество секунд для задержки перед запуском:" 0 0)
+
+    # Если пользователь нажал "Отмена" или "ESC", выходим из скрипта
+    if [ $? -ne 0 ]; then
+        echo "Отмена. Скрипт завершен."
+        exit 1
+    fi
+
+    # Проверяем, является ли введенное значение числом
+    if is_number "$delay"; then
+        dialog --msgbox "Вы ввели корректное количество секунд: $delay" 6 40
+        break
+    else
+        dialog --msgbox "Ошибка: Введите целое число." 6 40
+    fi
+done
+
+# Изменяем значение GRUB_TIMEOUT в файле /etc/default/grub
+sudo sed -i "s/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=$delay/" /etc/default/grub
+
+# Обновляем конфигурацию GRUB
+sudo update-grub
+
+# Выводим сообщение об успешном завершении
+dialog --msgbox "Задержка перед запуском установлена на $delay секунд. Конфигурация GRUB обновлена." 8 50
